4e458958d3bff650046062cdf97b1c66e1ad129f
efraimt <efraimt@gmail.com>
2022-03-14 21:22:52 +0200

    Add project files.

---
+++ Luach Hakefel/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+for (int j = 1; j <= 10; j++)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.Write(i * j + "   ");
+    }
+    Console.WriteLine();
+}
+
---
+++ Tirgul/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Please, enter 3 int numbers");
+int n1=int.Parse(Console.ReadLine());
+int n2=int.Parse(Console.ReadLine());
+int n3= int.Parse(Console.ReadLine());
+
+int max=n1, mid=n2;
+
+if (mid > max)
+{
+    max = n2;
+    mid = n1;
+}
+
+if (n3 > mid)
+{
+    if (n3 > max)
+    {
+        mid = max;
+        max = n3;
+    }
+    else { // n3 < max =>
+        mid = n3;
+    }
+}
+else { //n3<mid
+    ///לא צריך לעשות כלום
+    /// אפשר למחוק את הבלוק else
+
+}
+
+Console.WriteLine("The big number is: " + max);
+Console.WriteLine("The medium number is: " +mid);
+
+
